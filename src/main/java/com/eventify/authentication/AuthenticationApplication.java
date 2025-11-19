b0906e75e98ec97bb0c2ea91615e51967668d16a
achraf <achrafsikal@gmail.com>
2025-11-19 17:16:42 +0100

    feat: Integrate dotenv for dynamic database configuration
        This commit enhances the application by integrating the dotenv-java library to manage database configuration dynamically. This allows for environment-specific settings to be loaded from a `.env` file, improving security and flexibility.
    
        The following changes were made:
        - Added dotenv-java dependency in `pom.xml`.
        - Updated `AuthenticationApplication.java` to load database connection details from environment variables.
        - Removed hardcoded database configuration from `application.properties`.
        - Updated `.gitignore` to include `.env` file.
    
        This integration streamlines the configuration process and ensures sensitive information is not hardcoded, promoting best practices in application security.

--- src/main/java/com/eventify/authentication/AuthenticationApplication.java
+++ src/main/java/com/eventify/authentication/AuthenticationApplication.java
@@ -1,5 +1,6 @@
 package com.eventify.authentication;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthenticationApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		String postgresUri = String.format(
+				"jdbc:postgresql://%s:%s/%s?sslmode=require&channel_binding=require",
+				dotenv.get("PG_HOST"),
+				dotenv.get("PG_PORT"),
+				dotenv.get("PG_DB")
+		);
+
+		System.setProperty("spring.datasource.url", postgresUri);
+		System.setProperty("spring.datasource.username", dotenv.get("PG_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("PG_PASSWORD"));
+
 		SpringApplication.run(AuthenticationApplication.class, args);
 	}
 
