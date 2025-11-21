ea982db540f73fcb17f1dd789bdc3d28b1664b0d
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-11-21 16:19:14 +0100

    feat : add crud methods for Event Service and controller

--- src/main/java/com/eventify/authentication/controller/EventController.java
+++ src/main/java/com/eventify/authentication/controller/EventController.java
@@ -1,5 +1,37 @@
 package com.eventify.authentication.controller;
 
+
+import com.eventify.authentication.entity.Event;
+import com.eventify.authentication.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/events")
 public class EventController {
+
+    @Autowired
+    private  EventService eventService;
+
+    @GetMapping
+    ResponseEntity<List<Event>> getAllEvents() {
+        List<Event> all=eventService.getAll();
+        return new ResponseEntity<>(all, HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    Event creatEvent(@RequestBody Event event){
+        return eventService.creatEvent(event);
+    }
+
+    @GetMapping("s")
+    String gets()
+    {
+        return  "hello";
+    }
 
 }
--- src/main/java/com/eventify/authentication/services/EventService.java
+++ src/main/java/com/eventify/authentication/services/EventService.java
@@ -1,5 +1,55 @@
 package com.eventify.authentication.services;
 
+
+import com.eventify.authentication.entity.Event;
+import com.eventify.authentication.repository.EventRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class EventService {
+
+
+    private EventRepository eventRepository;
+
+    //create
+    public Event creatEvent(Event e){
+        return eventRepository.save(e);
+    }
+
+    //getAll
+    public List<Event> getAll()
+    {
+        return eventRepository.findAll();
+    }
+
+    //getById
+    public Event findById(UUID id){
+        return  eventRepository.findById(id)
+                .orElseThrow(()->new NotFoundException("the Event not exist in our database avec id = "+id));
+    }
+
+
+    //delete
+    public void deletById(UUID id){
+
+        eventRepository.deleteById(id);
+    }
+
+    //update
+    public Event updateEvent(Event e) {
+
+        Event updated=findById(e.getId());
+        updated.setId(e.getId());
+        updated.setCapacity(e.getCapacity());
+        updated.setOrganizer(e.getOrganizer());
+        updated.setLocation(e.getLocation());
+        updated.setDateTime(e.getDateTime());
+        updated.setRegistrations(e.getRegistrations());
+
+        return eventRepository.save(updated);
+    }
 
 }
