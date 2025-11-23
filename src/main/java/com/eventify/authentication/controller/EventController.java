7b22bf8a89e4a3387ade724a9e954192c57bea16
achraf <achrafsikal@gmail.com>
2025-11-23 19:35:04 +0100

    refactor: Remove unused EventController, RegistrationController, and ErrorDetails classes
        This commit removes the `EventController`, `RegistrationController`, and `ErrorDetails` classes from the project as they are no longer needed.
    
        The following changes were made:
        - Deleted `EventController` class.
        - Deleted `RegistrationController` class.
        - Deleted `ErrorDetails` class.
    
        This cleanup helps streamline the codebase by removing obsolete components, improving maintainability and reducing clutter in the project structure.

--- src/main/java/com/eventify/authentication/controller/EventController.java
+++
@@ -1,5 +0,0 @@
-package com.eventify.authentication.controller;
-
-public class EventController {
-
-}
--- src/main/java/com/eventify/authentication/controller/RegistrationController.java
+++
@@ -1,4 +0,0 @@
-package com.eventify.authentication.controller;
-
-public class RegistrationController {
-}
